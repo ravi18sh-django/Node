c5455e62db7c00a6333d19aa0fd152e99373f41e
Ravi Sharma <71383188+ravi18sh-django@users.noreply.github.com>
2024-09-21 22:29:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-//import express from "express";
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const app = express();
 
+// Define the port using process.env.PORT or default to 3000
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
-app.get('/twitter',(req, res)=>{
-    res.send('Cuckko')
-})
+app.get('/twitter', (req, res) => {
+    res.send('Cuckko');
+});
 
-app.get('/login',(req,res)=>{
-    res.send('<h1>Mahri Marzi</h1>')
-})
+app.get('/login', (req, res) => {
+    res.send('<h1>Mahri Marzi</h1>');
+});
 
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+// Use PORT variable in the listen method
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+});
